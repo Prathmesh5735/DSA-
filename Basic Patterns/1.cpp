abecbf239361e04e63fc8578dc2fb00a1805fddb
Prathmesh Chaudhari <140944700+Prathmesh5735@users.noreply.github.com>
2025-03-06 18:24:05 +0530

    done

---
+++ Basic Patterns/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {  // Loop for rows
+        for (int j = 1; j <= i; j++) {  // Loop for printing numbers
+            cout << j << " ";  
+        }
+        cout << endl;  
+    }
+    return 0;
+}
---
+++ Loop basics/fibonacci.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    int a=0;
+    int b=1;
+    int c;
+    cout << "Enter Your Number of n: ";
+    cin >> n;
+}
