55d5ae81c885de67c4fb7b0f8bac84b8628d1362
Prathmesh Chaudhari <140944700+Prathmesh5735@users.noreply.github.com>
2025-03-12 11:54:50 +0530

    done

---
+++ OOP 2/oop.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class Sport{
+	private :
+	int age;
+	string name,gameName,role;
+	
+	
+	public:
+	void input(int age, string name,string gameName, string role)
+	{
+		this->age=age;
+		this->name=name;
+		this->gameName=gameName;
+		this->role=role;
+	}
+	void output()
+	{
+		cout << "Name: "<< name << endl;
+		cout << "Age: "<< age << endl;
+		cout << "Game Name: "<< gameName << endl;
+		cout << "Role: "<< role << endl <<endl;
+		
+	}
+	
+};
+
+int main()
+{
+	int a=10;
+	Sport p1,p2,p3;
+	
+	p1.input(18,"Pintya","Cricket","Blower");
+	p1.output();
+	
+	p2.input(20,"Chintya","Cricket","keeper");
+	p2.output();
+	p3.input(18,"Bittya","Cricket","Better");
+	p3.output();
+	
+	return 0;
+}
+	
